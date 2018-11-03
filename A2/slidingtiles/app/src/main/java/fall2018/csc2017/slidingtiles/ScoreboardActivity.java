7039f844814e898df273c83d788463acd5b09f5d
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-03 14:09:39 -0400

    Almost finished ScoreboardActivity

--- A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/ScoreboardActivity.java
+++ A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/ScoreboardActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ScoreboardActivity extends AppCompatActivity {
 
@@ -11,22 +12,34 @@ public class ScoreboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoreboard);
+
+        //TODO: fix so that it goes to gamelauncher activity instead of game activity
+        Button menu = findViewById(R.id.MainMenuButton);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ScoreboardActivity.this, GameActivity.class));
+            }
+        });
+
+        Button myScoreboard = findViewById(R.id.MyScoreboardLabel);
+        myScoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ScoreboardActivity.this, UserScoreboardActivity.class));
+            }
+        });
+
+        Button sliding = findViewById(R.id.SlidingTilesScoreboardLabel);
+        sliding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ScoreboardActivity.this, SlidingScoreboardActivity.class));
+            }
+        });
     }
 
-    // TODO: fix the activity so that it is redirected to game launcher activity
-    public void goToMainMenu (View view){
-        Intent intent = new Intent (this, GameActivity.class);
-        startActivity(intent);
-    }
-    public void goToUserScoreboard (View view){
-        Intent intent = new Intent (this, UserScoreboardActivity.class);
-        startActivity(intent);
-    }
-    //TODO: implement the slidingscoreboardactivity
-    public void goToSlidingTilesScoreboard (View view){
-        Intent intent = new Intent (this, SlidingScoreboardActivity.class);
-        startActivity(intent);
-    }
+
 
 
 
