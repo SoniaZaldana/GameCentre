234b851e7bf653d879c136b605bc1ab3bcebaa2b
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-03 21:17:37 -0400

    More edits to Game Launcher Activity

--- A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/GameLauncherActivity.java
+++ A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/GameLauncherActivity.java
@@ -1,28 +1,29 @@
 package fall2018.csc2017.slidingtiles;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 
-public class Gamelauncheractivity extends AppCompatActivity implements View.OnClickListener {
+public class GameLauncherActivity extends AppCompatActivity implements View.OnClickListener {
     Button SignOff;
+    Button Scoreboards;
     ImageView TilesGame;
     String UsersInfo;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamelauncheractivity);
+
         UsersInfo = getIntent().getStringExtra("Username");
         TilesGame = findViewById(R.id.TilesGame);
         SignOff = findViewById(R.id.signoff);
+        Scoreboards = findViewById(R.id.ScoreboardButton);
         TilesGame.setOnClickListener(this);
         SignOff.setOnClickListener(this);
+        Scoreboards.setOnClickListener(this);
 
     }
     @Override
@@ -36,7 +37,10 @@ public class Gamelauncheractivity extends AppCompatActivity implements View.OnCl
             case R.id.signoff:
                 startActivity(new Intent(this, Loginactivity.class));
                 break;
-
+            case R.id.ScoreboardButton:
+                startActivity(new Intent(this,
+                        ScoreboardActivity.class).putExtra("Username", UsersInfo));
+                break;
         }
     }
 }
