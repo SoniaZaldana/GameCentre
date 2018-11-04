0f566eaecba14686cb1c2cc2d8fde2a19ea6b7f8
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-04 10:30:57 -0500

    I just changed some spacing.

--- A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/RegisteractivityActivity.java
+++ A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/RegisteractivityActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class RegisteractivityActivity extends AppCompatActivity implements View.OnClickListener {
     Button RegisterButton, BackToLogIn;
     EditText eUsername, ePassword, reEnteredPassword;
