ecca69e36ae87f8afdb04fbe4c792a7ff3b47242
Osman <osman.afandiyev@mail.utoronto.ca>
2018-11-06 01:27:27 +0400

    Gamelauncher GUI advanced

--- A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/Gamelauncheractivity.java
+++ A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/Gamelauncheractivity.java
@@ -9,9 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.ToLongBiFunction;
 
 public class Gamelauncheractivity extends AppCompatActivity implements View.OnClickListener {
     Button SignOff;
@@ -24,11 +26,13 @@ public class Gamelauncheractivity extends AppCompatActivity implements View.OnCl
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamelauncheractivity);
+
         UsersInfo = getIntent().getStringExtra("Username");
         currentUsername = getApplicationContext().getSharedPreferences("sharedUser", MODE_PRIVATE);
         editor = currentUsername.edit();
         editor.putString("thisUser", UsersInfo);
         editor.apply();
+//        setSupportActionBar((android.support.v7.widget.Toolbar) findViewById(R.id.toolbar));
         TilesGame = findViewById(R.id.TilesGame);
         SignOff = findViewById(R.id.signoff);
         Scoreboards = findViewById(R.id.ScoreboardButton);
@@ -39,8 +43,6 @@ public class Gamelauncheractivity extends AppCompatActivity implements View.OnCl
             //TODO: change this when osman's null error is fixed
             ContextWrapper cw = new ContextWrapper(this);
             File directory = cw.getDir("media", MODE_PRIVATE);
-
-
             File userScoreFile = new File( "Sonia.txt");
             File slidingFile = new File("SlidingTiles.txt");
             userScoreFile.createNewFile();
@@ -53,7 +55,6 @@ public class Gamelauncheractivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()) {
             case R.id.TilesGame:
                 startActivity(new Intent(this,
