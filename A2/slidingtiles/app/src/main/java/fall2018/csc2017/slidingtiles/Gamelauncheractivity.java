9ab0e4a8051892914e9164efbf59af5f62331c3a
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-05 16:23:33 -0500

    Edited gamelauncher

--- A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/Gamelauncheractivity.java
+++ A2/slidingtiles/app/src/main/java/fall2018/csc2017/slidingtiles/Gamelauncheractivity.java
@@ -1,15 +1,11 @@
 package fall2018.csc2017.slidingtiles;
 
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -18,18 +14,13 @@ public class Gamelauncheractivity extends AppCompatActivity implements View.OnCl
     Button Scoreboards;
     ImageView TilesGame;
     String UsersInfo;
-    SharedPreferences currentUsername;
-    SharedPreferences.Editor editor;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamelauncheractivity);
-
         UsersInfo = getIntent().getStringExtra("Username");
-        currentUsername = getApplicationContext().getSharedPreferences("sharedUser", MODE_PRIVATE);
-        editor = currentUsername.edit();
-        editor.putString("thisUser", UsersInfo);
-        editor.apply();
+        SharedPreferenceManager.setSharedValue(this, "sharedUser", "thisUser", UsersInfo);
 
         TilesGame = findViewById(R.id.TilesGame);
         SignOff = findViewById(R.id.signoff);
