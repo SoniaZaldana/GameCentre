3900b486d62b02f40d04ed713b3fda1d655109f6
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-10 18:03:32 -0500

    Deleted an unnecessary boolean parameter that kept showing up

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/GestureDetectGridView.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/GestureDetectGridView.java
@@ -20,7 +20,7 @@ public class GestureDetectGridView extends GridView {
     public static final int SWIPE_MAX_OFF_PATH = 100;
     public static final int SWIPE_THRESHOLD_VELOCITY = 100;
     private GestureDetector gDetector;
-    private MovementController mController;
+    private MovementControllerSliding mController;
     private boolean mFlingConfirmed = false;
     private float mTouchX;
     private float mTouchY;
@@ -49,7 +49,7 @@ public class GestureDetectGridView extends GridView {
     }
 
     private void init(final Context context) {
-        mController = new MovementController();
+        mController = new MovementControllerSliding();
         gDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
 
             @Override
@@ -57,7 +57,7 @@ public class GestureDetectGridView extends GridView {
                 int position = GestureDetectGridView.this.pointToPosition
                         (Math.round(event.getX()), Math.round(event.getY()));
 
-                mController.processTapMovement(context, position, true);
+                mController.processTapMovement(context, position);
                 return true;
             }
 
