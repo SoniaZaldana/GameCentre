7008ff08fcb9f939a8e061fc1557b386a6b3a097
alexu <alexu.wong@mail.utoronto.ca>
2018-11-17 18:46:43 -0500

    Added a timer class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
@@ -1,6 +1,46 @@
 package fall2018.csc2017.GameCentre.MineSweeper;
+
+import java.util.TimerTask;
+
 //TODO Create a display for the flag counter, and get it from MovementControllerSweeper.
 //TODO WIll probably have to implement observable.
 public class MineSweeperActivity {
+
+
+
+
+
+    /**
+     * A timer task that increments the timer.
+     */
+    private class ScoreTask extends TimerTask {
+
+        /**
+         * The boardmanager that uses this timer.
+         */
+        private SweeperBoardManager manager;
+
+        /**
+         * An task that increments the time.
+         *
+         * @param manager The board manager this is acting on
+         */
+        public ScoreTask(SweeperBoardManager manager){
+            super();
+            this.manager = manager;
+        }
+
+        /**
+         * The task this timer does.
+         */
+        public void run(){
+            this.manager.timeIncrement();
+        }
+
+
+
+    }
+
+
 
 }
--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/SweeperBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/SweeperBoardManager.java
@@ -3,6 +3,26 @@ package fall2018.csc2017.GameCentre.MineSweeper;
 import fall2018.csc2017.GameCentre.BoardManager;
 
 public class SweeperBoardManager extends BoardManager<SweeperTilesBoard> {
+    /**
+     * The number of seconds that has passed for this game
+     */
+    private int time;
+
+    /**
+     * A getter for the time
+     * @return time time that has transpired for the game
+     */
+    public int getTime() {
+        return time;
+    }
+
+    /**
+     * Increments the time by 1.
+     */
+    public void timeIncrement(){
+        this.time++;
+    }
+
     @Override
     public int calculateScore(int moves) {
         return 0;
