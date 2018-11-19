e7b7a44e20b2f6c693c779150ac4609b5837bb48
alexu <alexu.wong@mail.utoronto.ca>
2018-11-19 14:46:29 -0500

    changed MineSweeperActivity

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
@@ -1,19 +1,22 @@
 package fall2018.csc2017.GameCentre.MineSweeper;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 //TODO Create a display for the flag counter, and get it from MovementControllerSweeper.
 //TODO WIll probably have to implement observable.
-public class MineSweeperActivity {
+public class MineSweeperActivity extends AppCompatActivity {
     private Timer timer = new Timer();
 
     private SweeperBoardManager sweeperBoardManager;
 
     @Override
-    protected void onCreate(){
-        super.onCreate();
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         addSecond();
 
     }
