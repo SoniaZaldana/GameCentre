2786308190558f3925cf460e6d8011550b17c604
alexu <alexu.wong@mail.utoronto.ca>
2018-11-24 13:01:08 -0500

    Not sure what was changed but I need to commit before I pull

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/MineSweeperActivity.java
@@ -3,21 +3,43 @@ package fall2018.csc2017.GameCentre.MineSweeper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import fall2018.csc2017.GameCentre.CustomAdapter;
+import fall2018.csc2017.GameCentre.GestureDetectGridViews.GestureDetectGridViewLongPress;
 
 
 //TODO Create a display for the flag counter, and get it from MovementControllerSweeper.
 //TODO WIll probably have to implement observable.
-public class MineSweeperActivity extends AppCompatActivity {
+public class MineSweeperActivity extends AppCompatActivity implements Observer {
+    private GestureDetectGridViewLongPress gridView;
+
     private Timer timer = new Timer();
 
     private SweeperBoardManager sweeperBoardManager;
+
+    /**
+     * Set up the background image for each button based on the master list
+     * of positions, and then call the adapter to set the view.
+     */
+    // Display
+    public void display() {
+        updateTime();
+        gridView.setAdapter(new CustomAdapter(tileButtons, columnWidth, columnHeight));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         sweeperBoardManager.startTimer();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        display();
     }
 
     @Override
