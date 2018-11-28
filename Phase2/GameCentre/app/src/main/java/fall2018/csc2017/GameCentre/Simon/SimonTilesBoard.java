ce2e8942e7bad67a0b96fb89ea4d4f247bdf80c6
Nicolae <nicolae.stroncea@mail.utoronto.ca>
2018-11-27 22:58:23 -0500

    added new tests

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Simon/SimonTilesBoard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Simon/SimonTilesBoard.java
@@ -1,6 +1,10 @@
 package fall2018.csc2017.GameCentre.Simon;
 
+import android.content.res.TypedArray;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import fall2018.csc2017.GameCentre.Board;
 import fall2018.csc2017.GameCentre.Tile;
@@ -15,6 +19,16 @@ public class SimonTilesBoard extends Board<SimonTile> {
 
     public SimonTilesBoard(int dimension, List<SimonTile> tiles) {
         super(dimension, tiles);
+    }
+//TODO implement it in the activity. Check if it is correct
+    public void setColorsForAllTiles(List<Integer> listOfTileColors){
+        int counter = 0;
+        for(List<SimonTile> arr: getAllTiles()){
+            for(SimonTile t: arr){
+                t.setColor(listOfTileColors.remove(counter));
+                counter+=1;
+            }
+        }
     }
 
 }
--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/Simon/GameQueueTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/Simon/GameQueueTest.java
@@ -1,6 +1,11 @@
 package fall2018.csc2017.GameCentre.Simon;
 
+import android.os.Handler;
+
 import org.junit.Test;
+
+import java.util.ListIterator;
+
 import static org.junit.Assert.*;
 
 public class GameQueueTest {
@@ -22,11 +27,65 @@ public class GameQueueTest {
     }
 
     @Test
+    public void size(){
+        GameQueue<SimonTile> queue = new GameQueue<>();
+        for(int i = 0; i<4; i++){
+            queue.add(new SimonTile());
+        }
+        assertEquals(4,queue.getSize() );
+    }
+    @Test
+    public void get(){
+        GameQueue<SimonTile> queue = new GameQueue<>();
+        SimonTile t1 = new SimonTile(0);
+        SimonTile t2 = new SimonTile(1);
+        queue.add(t1);
+        queue.add(t2);
+        assertEquals(1, queue.get(1).getId());
+        final SimonBoardManager sbm = new SimonBoardManager(3,2 );
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SimonTile randomTile = sbm.randomizer();
+                sbm.getGameQueue().add(randomTile);
+            }
+        }, 1000);
+    }
+
+    @Test
     public void remove() {
         GameQueue<SimonTile> queue = new GameQueue<>();
         SimonTile tile = new SimonTile();
         queue.add(tile);
         assertEquals(tile, queue.remove());
         assertEquals(true, queue.isEmpty());
+    }
+    @Test
+    public void testIterator(){
+        GameQueue<SimonTile> queue = new GameQueue<>();
+        ListIterator<SimonTile> it = queue.iterator();
+        //text hasPrevious and hasNext
+        assertFalse(it.hasPrevious());
+        assertFalse(it.hasNext());
+        SimonTile tile1 = new SimonTile(1);
+        queue.add(tile1);
+        assertTrue(it.hasNext());
+        assertEquals(it.next(), tile1);
+        assertEquals(it.previous(), tile1);
+        SimonTile tile2 = new SimonTile(2);
+        queue.add(tile2);
+        SimonTile tile3 = new SimonTile(3);
+        queue.add(tile3);
+        //text hasNext on a 3rd item
+        it = queue.iterator();
+        it.next();
+        it.next();
+        assertEquals(tile3, it.next());
+        assertEquals(2, it.previousIndex());
+        assertEquals(4, it.nextIndex());
+
+
+
     }
 }
