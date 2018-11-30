529902370899cd1f99cd60c934697371bc7f1456
Sonia Zaldana Calles <sonia.zaldana@mail.utoronto.ca>
2018-11-29 21:11:58 -0500

    Added constructor that Osman deleted

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/SweeperTilesBoard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/MineSweeper/SweeperTilesBoard.java
@@ -3,6 +3,7 @@ package fall2018.csc2017.GameCentre.MineSweeper;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 import fall2018.csc2017.GameCentre.Board;
@@ -22,6 +23,15 @@ class SweeperTilesBoard extends Board<SweeperTile> {
      * The amount of time a time bomb has before exploding
      */
     private int bombTime;
+
+    SweeperTilesBoard(int dimension, List<SweeperTile> listOfTiles) {
+        super(dimension,listOfTiles);
+        this.time = 0;
+        this.hitPoints = 3;
+        this.bombTime = 10;
+        hasChanged();
+        notifyObservers();
+    }
 
     SweeperTilesBoard(int dimension, int complexity) {
         super();
